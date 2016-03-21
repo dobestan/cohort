c9755c0ef54f52a318d28d5776685bb7c18618fd
Suchan An <dobestan@gmail.com>
2016-03-21 11:31:18 +0900

    update cohort.sql: add cohort_size table

--- cohort.sql
+++ cohort.sql
@@ -10,3 +10,14 @@ FROM
     rental r
 GROUP BY 1
 ;
+
+# Cohort Size
+DROP TEMPORARY TABLE IF EXISTS cohort_size;
+CREATE TEMPORARY TABLE cohort_size
+SELECT
+    LEFT(first_time, 7) Month,
+    COUNT(*) num
+FROM
+    first_rental
+GROUP BY 1
+;
